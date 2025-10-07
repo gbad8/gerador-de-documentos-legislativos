9bdbfbbd55ca105b707c3ee0e3cc1721baa37c57
Guilherme d'Almeida <guilhermebarros181@gmail.com>
2025-10-07 17:20:42 -0300

    feat: initializing translation into c#

---
+++ Models/Indicacao.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeradorDeDocumentosLegislativos.Models
+{
+    public class Indicacao
+    {
+        public int LetterNumber { get; set; }
+        public DateOnly ApprovalDate { get; set; }
+        public int PropositionNumber { get; set; }
+        public string Subject { get; set; } = string.Empty;
+        public string Request { get; set; } = string.Empty;
+        public bool IsJoint { get; set; }
+        public string AuthorName { get; set; }  = string.Empty;
+        public string ApprovalQuorum { get; set; } = string.Empty;
+        public int SessionNumber { get; set; }
+
+        public void PrintPdf()
+        {
+            Console.WriteLine($"A Indicação n°{PropositionNumber} foi aprovada!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using GeradorDeDocumentosLegislativos.Models;
+
+DateTime todaysDate = DateTime.Now;
+string dateForImpression = todaysDate.ToString("d 'de' MMMM 'de' yyyy");
+
+Indicacao indicacaoTeste = new Indicacao();
+indicacaoTeste.PropositionNumber = 63;
+indicacaoTeste.PrintPdf();
