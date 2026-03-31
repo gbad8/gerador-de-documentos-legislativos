948498e788a7fd0528fe47b44f31c40835284a7a
Guilherme dAlmeida <guilhermebarros181@gmail.com>
2026-03-30 21:35:52 -0300

    feat: add database migration for initial domain models including Autores, Camaras, Usuarios, Numeracoes, and Oficios

---
+++ Gdl.Solution/Gdl.Web/Infrastructure/Data/Migrations/20260331003537_AddDomainModels.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gdl.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDomainModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
