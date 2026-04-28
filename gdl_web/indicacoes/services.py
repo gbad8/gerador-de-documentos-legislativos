d3bbf19c3996bb58cfbed00cefd7474589332a9d
Guilherme dAlmeida <guilhermebarros181@gmail.com>
2026-04-27 21:16:09 -0300

    feat: update legislative indication PDF layout and include chamber president details

--- gdl_web/indicacoes/services.py
+++ gdl_web/indicacoes/services.py
@@ -4,9 +4,13 @@ class PdfService:
     @staticmethod
     def gerar_indicacao(indicacao, camara):
         from weasyprint import HTML
+        from autores.models import Autor
+
+        presidente = Autor.objects.filter(camara=camara, cargo=Autor.Cargo.PRESIDENTE).first()
 
         html_string = render_to_string("indicacoes/indicacao_pdf.html", {
             "indicacao": indicacao,
             "camara": camara,
+            "presidente": presidente,
         })
         return HTML(string=html_string).write_pdf()
